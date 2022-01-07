532458de8d4b8a987282e67f60a807b4df48d512
Roos Cocolien <r.steigenga@gmail.com>
2022-01-07 15:01:05 +0100

    page-content: update class names

--- src/components/PageContent/PageContent.vue
+++ src/components/PageContent/PageContent.vue
@@ -1,18 +1,18 @@
 <template>
-	<article :class="reverse ? 'content__reverse' : 'content'">
-		<img class="content__image"
+	<article :class="reverse ? 'page-content__reverse' : 'page-content'">
+		<img class="page-content__image"
 			src="https://via.placeholder.com/580x430"
 			alt="Person with face mask"
 		/>
-		<article class="content__text">
+		<article class="page-content__text">
 			<a href="#" class="link link__brand">Reference &#38; decision support solutions &gt;</a>
 			<h3>Improve patient outcomes with clinical and drug references</h3>
 			<p>
 				Accessible evidence based content for all stages of a patient care.
 			</p>
-			<div class="content__icon">
-				<h5><abbr class="content__icon--abbr">CK</abbr></h5>
-				<h5 class="content__icon--text">ClinicalKey</h5>
+			<div class="page-content__icon">
+				<h5><abbr class="page-content__icon--abbr">CK</abbr></h5>
+				<h5 class="page-content__icon--text">ClinicalKey</h5>
 			</div>
 		</article>
 	</article>
--- src/components/TopicsCard/TopicsCard.vue
+++ src/components/TopicsCard/TopicsCard.vue
@@ -22,6 +22,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style src="./TopicsCard" lang="scss"></style>
